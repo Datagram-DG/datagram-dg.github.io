efb1a89e4a11e2837965488a5b01090c94f5d273
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-24 10:59:17 +0530

    Update config.js

--- data/js/config.js
+++ data/js/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyC-ZtQ4N3d5ZYZcUCETe0quVgn3wD8Gdq4",
-    authDomain: "datagram-in.firebaseapp.com",
-    databaseURL: "https://datagram-in-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "datagram-in",
-    storageBucket: "datagram-in.appspot.com",
-    messagingSenderId: "984986151374",
-    appId: "1:984986151374:web:8428c23ad9e8bd88ee639f"
+    apiKey: "AIzaSyCejjyfBWLw6A9stFkfud6tWKpW7CfGnOI",
+    authDomain: "datagram-india.firebaseapp.com",
+    databaseURL: "https://datagram-india-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "datagram-india",
+    storageBucket: "datagram-india.appspot.com",
+    messagingSenderId: "771780316207",
+    appId: "1:771780316207:web:0326c993df64b561a64ada"
 };
